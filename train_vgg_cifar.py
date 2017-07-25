f7b74f057c74899007cab39251707758e565547e
vincentherrmann <vincent.herrmann@web.de>
2017-07-25 15:50:46 +0200

    add script arguments and cuda check

--- train_vgg_cifar.py
+++ train_vgg_cifar.py
@@ -1,19 +1,37 @@
 import torch
-import os
+import argparse
 from vgg import VGG
 from training import OptimizerMNIST, OptimizerCIFAR10
-from expanding_modules import Conv1dExtendable
 from logger import Logger
 
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--epochs', default=20, type=int, help='training epochs')
+parser.add_argument('--lr', default=0.001, type=float, help='learning rate')
+parser.add_argument('--exp_itv', default=200, type=int, help='expand interval')
+parser.add_argument('--exp_rate', default=4, type=int, help='expand rate')
+parser.add_argument('--pr_rate', default=0, type=int, help='prune rate')
+
+args = parser.parse_args()
+
+print(args)
+
+cuda = torch.cuda.is_available()
+
 model = VGG()
+if cuda:
+    model.cuda()
+
 trainer = OptimizerCIFAR10(model,
-                           epochs=20,
-                           expand_interval=200,
+                           epochs=args.epochs,
+                           expand_interval=args.exp_itv,
                            log_interval=400,
-                           expand_rate=15,
-                           prune_rate=0,
-                           lr=0.001,
-                           weight_decay=0)
+                           expand_rate=args.exp_rate,
+                           prune_rate=args.pr_rate,
+                           lr=args.lr,
+                           weight_decay=0,
+                           cuda=cuda)
+
+#print("exp_rate: ", trainer.expand_rate)
 
 #name = str(model.layer_count) + "_layers_" + "_extend_" + str(trainer.extend_threshold) + "_prune_" + str(trainer.prune_threshold) + "_Adam"
 name = str(model.layer_count) + "_layers_" + "_expand_" + str(trainer.expand_rate) + "_prune_" + str(trainer.prune_rate) + "_interval_" + str(trainer.expand_interval) + "_lr_" + str(trainer.lr) + "test"
